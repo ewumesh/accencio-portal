64b08a11a1ee1ca21bb084af6c153ef44d4d992b
daniel.david <daniel.david@bissoft.ro>
2021-02-19 12:17:46 +0200

    rm visualizer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   API_GATEWAY: 'https://hmdz1lq98a.execute-api.us-east-1.amazonaws.com/Prod',
-  TABLEAU_API: 'https://visualize.accencio.com',
-  SPOTFIRE_API: 'https://visualizer.accencio.com/spotfire/wp'
+  TABLEAU_API: 'https://apps.accencio.com',
+  SPOTFIRE_API: 'https://apps.accencio.com/spotfire/wp'
 };
